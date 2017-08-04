c53d27c755471cf580b2086c4ee95090eb4bb8a1
brandon <pokemonbrandon1234@gmail.com>
2017-08-04 21:40:17 +0100

    Added a search function.

--- src/main.py
+++ src/main.py
@@ -12,3 +12,10 @@ if crunchyLoginOutput == "True": #If the crunchyLoginOutput returns True, it'll 
     print("Login Successful")
 else:
     print("Login Failed")
+
+userSearchInput = input("Search for a show: ") #Asks the user to input what show they want to look for.
+
+userSearchOutput = api.search_anime_series(userSearchInput)
+
+for names in range(len(userSearchOutput)):
+    print(userSearchOutput[names])
