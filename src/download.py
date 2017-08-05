9ec459d1b1fd847abdb370b855341c4b549fbc70
Mar2ck <marcchester7@gmail.com>
2017-08-05 14:06:12 +0100

    Add ability to download videos in 480p

--- src/download.py
+++ src/download.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python3
+from __future__ import unicode_literals
 import cfscrape
+import youtube_dl
+
 
 CRUsername = input("Crunchyroll Username: ") #Asks the user to enter their username.
 CRPassword = input("Crunchyroll Password: ") #Asks the user to enter their password.
 
-payload = {
-    'name': CRUsername,
-    'password': CRPassword
-}
+selectedEpisode = "http://www.crunchyroll.com/new-game/episode-1-it-actually-feels-like-i-started-my-job-715393"
 
-with cfscrape.create_scraper() as s:
-    p = s.get("https://m.crunchyroll.com/login")
-    p = s.post("https://m.crunchyroll.com/?a=formhandler", data=payload)
-    print(p.text)
-    
-    #r = s.get("http://www.crunchyroll.com/home/queue")
-    #print (r.text)
+#payload = {
+#    'name': CRUsername,
+#   'password': CRPassword
+#}
+#
+#with cfscrape.create_scraper() as s:
+#    p = s.get("https://m.crunchyroll.com/login")
+#    p = s.post("https://m.crunchyroll.com/?a=formhandler", data=payload)
+#    print(p.text)
+#    
+#    r = s.get("http://www.crunchyroll.com/home/queue")
+#    print (r.text)
+
+ydl_opts = {}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([selectedEpisode])
--- src/main.py
+++ src/main.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python3
 #Created on 04/08/2017. All the background stuff is here.
+from __future__ import unicode_literals
 from crunchyroll.apis.meta import MetaApi
+import cfscrape
+import youtube_dl
 api = MetaApi()
 
+doLoginOrNot = input("Do you want to login: ")
+if doLoginOrNot == "yes":
+    #The main code.
+    CRUsername = input("Crunchyroll Username: ") #Asks the user to enter their username.
+    CRPassword = input("Crunchyroll Password: ") #Asks the user to enter their password.
 
-#The main code.
-CRUsername = input("Crunchyroll Username: ") #Asks the user to enter their username.
-CRPassword = input("Crunchyroll Password: ") #Asks the user to enter their password.
-
-crunchyLoginOutput = api.login(username=CRUsername, password=CRPassword) # Logs into Crunchyroll
-
-#if crunchyLoginOutput == "True": #If the crunchyLoginOutput returns True, it'll print that is successful.
-#    print("Login Successful")
-#else:
-#    print("Login Failed")
+    crunchyLoginOutput = api.login(username=CRUsername, password=CRPassword) # Logs into Crunchyroll
 
 userSearchInput = input("Search for a show: ") #Asks the user to input what show they want to look for.
 userSearchOutput = api.search_anime_series(userSearchInput)
@@ -34,6 +33,9 @@ if confirmation == "yes": #If yes it will return the episodes.
     ep = [e for e in userEpisodes if e.episode_number == episodeInput][0] #Simple list comprehension, returns the actual episode the user is trying to view.
     theURLForTheStream = ep.url
     print(theURLForTheStream)
+    ydl_opts = {}
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([theURLForTheStream])
 
 else: #Else it will print bye and quit.
     print("Bye bye")
